4a5ffcc60dafca717170bd1ab63d535cdaa81b59
iseunghag <qw0802@naver.com>
2023-07-13 21:02:13 +0900

    #5 feat:db생성,yml설정

--- src/main/java/com/shallwe/ShallWeApplication.java
+++ src/main/java/com/shallwe/ShallWeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:database/application-database.yml"}, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = {"classpath:application.yml"}, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = { "classpath:swagger/application-springdoc.yml" }, factory = YamlPropertySourceFactory.class)
 public class ShallWeApplication {
 
