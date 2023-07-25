9b941c201174d91077d351ac4f801d2c653867af
iseunghag <qw0802@naver.com>
2023-07-26 04:52:35 +0900

    #19 fix:도커파일 수정

--- src/main/java/com/shallwe/ShallWeApplication.java
+++ src/main/java/com/shallwe/ShallWeApplication.java
@@ -3,9 +3,7 @@ package com.shallwe;
 import com.shallwe.global.config.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @PropertySource(value = {"classpath:database/application-database.yml"}, factory = YamlPropertySourceFactory.class)
