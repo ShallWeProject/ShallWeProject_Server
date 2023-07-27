18117c7a17e7dcd5b443bc730281f01c46c8e09a
박세진 <jinpark99@naver.com>
2023-07-27 16:13:00 +0900

    chore: SignUpUserReq 스웨거 설명 추가

--- src/main/java/com/shallwe/domain/user/dto/SignUpUserReq.java
+++ src/main/java/com/shallwe/domain/user/dto/SignUpUserReq.java
@@ -1,15 +1,17 @@
 package com.shallwe.domain.user.dto;
 
 import com.shallwe.domain.user.domain.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class SignUpUserReq {
 
+    @Schema(type = "boolean", example = "true / false", description = "마켓팅 수신 동의 여부")
     private Boolean marketingConsent;
-
+    @Schema(type = "int", example = "25", description = "나이")
     private Integer age;
-
+    @Schema(type = "string", example = "MALE / FEMALE / UNKNOWN", description = "성별")
     private Gender gender;
 
 }
