67b5ec1193ce9810a40d2401387e22b83c7062fc
hyunw9 <rkdgusdnr32@naver.com>
2023-07-28 10:15:28 +0900

    refactor: Repository 수정

--- src/main/java/com/shallwe/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/shallwe/domain/reservation/domain/repository/ReservationRepository.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findAllByUserId(Long userId);
+
 }
