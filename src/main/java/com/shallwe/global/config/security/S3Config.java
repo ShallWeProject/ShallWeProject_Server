c9a3e536be09d89aa43cdd4ae568de9e42617d99
hyunw9 <rkdgusdnr32@naver.com>
2023-08-05 23:47:09 +0900

    Feat: 파일 업로드 설정 추가

--- src/main/java/com/shallwe/global/config/security/S3Config.java
+++ src/main/java/com/shallwe/global/config/security/S3Config.java
@@ -18,7 +18,7 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client(){
+    public AmazonS3Client amazonS3(){
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
