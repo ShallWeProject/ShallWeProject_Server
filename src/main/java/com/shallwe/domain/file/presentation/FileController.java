5f623058a6e50ebe8bc20089c747374dac4f959d
hyunw9 <rkdgusdnr32@naver.com>
2023-08-06 21:44:19 +0900

    feat: presigned URL 반환 컨트롤러 추가

--- src/main/java/com/shallwe/domain/file/presentation/FileController.java
+++ src/main/java/com/shallwe/domain/file/presentation/FileController.java
@@ -1,26 +1,47 @@
 package com.shallwe.domain.file.presentation;
 
-import com.shallwe.domain.file.application.FileUploadService;
+import com.shallwe.domain.file.application.S3Service;
+import com.shallwe.domain.user.dto.UserDetailRes;
+import com.shallwe.global.config.security.token.CurrentUser;
+import com.shallwe.global.config.security.token.UserPrincipal;
+import com.shallwe.global.payload.ErrorResponse;
 import com.shallwe.global.payload.ResponseCustom;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import java.net.URL;
 
 @RestController
-@RequestMapping(value = "/upload")
+@RequestMapping(value = "api/v1/file")
 @RequiredArgsConstructor
 public class FileController {
-    private final FileUploadService fileUploadService;
+    private final S3Service s3Service;
 
-    @PostMapping
+    /*@PostMapping
     public ResponseCustom<String> uploadFile(
             @RequestParam("images") MultipartFile multipartFile)throws IOException {
-        return ResponseCustom.OK(fileUploadService.upload(multipartFile));
+        return ResponseCustom.OK(s3Service.upload(multipartFile));
+    }*/
+    @Operation(summary = "PresignedURL 생성", description = "파일 업로드를 위한 PresignedURL을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Presigned URl 생성 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserDetailRes.class))}),
+            @ApiResponse(responseCode = "400", description = "Presigned URL 생성 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @PostMapping("/generate-presigned-url")
+    public ResponseCustom<String> generatedPresignedURL(
+            @Parameter(description = "fileName을 입력해주세요", required = true) @RequestParam("fileName") String fileName
+    ) {
+        String objectKey= "uploads/" + fileName; //S3 대상 경로 지정
+        URL presignedURL = s3Service.generatePresignedUrl(objectKey,30);
+        return ResponseCustom.OK(presignedURL.toString());
     }
 
 
