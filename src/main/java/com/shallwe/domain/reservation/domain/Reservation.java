c3cc504310a2b4d2d8b70e90cea657ee8999dd9e
hyunw9 <rkdgusdnr32@naver.com>
2023-08-13 20:38:10 +0900

    refactor: 엔티티 구조 변경

--- src/main/java/com/shallwe/domain/reservation/domain/Reservation.java
+++ src/main/java/com/shallwe/domain/reservation/domain/Reservation.java
@@ -30,15 +30,17 @@ public class Reservation extends BaseEntity {
     @JoinColumn(name = "experience_gift_id")
     private ExperienceGift experienceGift;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
 
     private Long persons;
 
     private LocalDateTime date;
 
-    private String receiver;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
 
     private String phoneNumber;
 
@@ -53,10 +55,10 @@ public class Reservation extends BaseEntity {
     List<MemoryPhoto> memoryPhotos = new ArrayList<>();
 
     @Builder
-    public Reservation(Long id, ExperienceGift experienceGift, User user, Long persons, LocalDateTime date, String receiver, String phoneNumber, String invitationImg, String invitationComment, ReservationStatus reservationStatus) {
+    public Reservation(Long id, ExperienceGift experienceGift, User sender, Long persons, LocalDateTime date, User receiver, String phoneNumber, String invitationImg, String invitationComment, ReservationStatus reservationStatus) {
         this.id = id;
         this.experienceGift = experienceGift;
-        this.user = user;
+        this.sender = sender;
         this.persons = persons;
         this.date = date;
         this.receiver = receiver;
@@ -69,7 +71,7 @@ public class Reservation extends BaseEntity {
     public void updateReservation(UpdateReservationReq updateReq) {
         this.persons = Optional.ofNullable(updateReq.getPersons()).orElse(this.persons);
         this.date = Optional.ofNullable(updateReq.getDate()).orElse(this.date);
-        this.receiver = Optional.ofNullable(updateReq.getReceiver()).orElse(this.receiver);
+        this.receiver = Optional.ofNullable(updateReq.getReceiver()).orElse(this.getReceiver());
         this.phoneNumber = Optional.ofNullable(updateReq.getPhone_number()).orElse(this.phoneNumber);
         this.invitationImg = Optional.ofNullable(updateReq.getInvitation_img()).orElse(this.invitationImg);
         this.invitationComment = Optional.ofNullable(updateReq.getInvitation_comment()).orElse(this.invitationComment);
---
+++ src/main/java/com/shallwe/domain/user/exception/InvalidPhoneNumberException.java
@@ -0,0 +1,7 @@
+package com.shallwe.domain.user.exception;
+
+public class InvalidPhoneNumberException extends RuntimeException{
+    public InvalidPhoneNumberException(){
+        super("해당 전화번호의 유저가 없습니다.");
+    }
+}
