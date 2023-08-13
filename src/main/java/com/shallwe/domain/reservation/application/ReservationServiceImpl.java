bc91d007188f6b427dad918d342293576a7b982c
hyunw9 <rkdgusdnr32@naver.com>
2023-08-13 20:44:02 +0900

    refactor: 코드 리팩토링
    기존에 있었던 불필요한 변수및 수식 제거,
    람다식 적용

--- src/main/java/com/shallwe/domain/reservation/application/ReservationServiceImpl.java
+++ src/main/java/com/shallwe/domain/reservation/application/ReservationServiceImpl.java
@@ -1,5 +1,9 @@
 package com.shallwe.domain.reservation.application;
 
+import com.shallwe.domain.experience_gift.domain.ExperienceGift;
+import com.shallwe.domain.experience_gift.dto.response.ExperienceExpCategoryRes;
+import com.shallwe.domain.experience_gift.exception.ExperienceGiftNotFoundException;
+import com.shallwe.domain.experience_gift.repository.ExperienceGiftRepository;
 import com.shallwe.domain.reservation.domain.Reservation;
 import com.shallwe.domain.reservation.domain.repository.ReservationRepository;
 import com.shallwe.domain.reservation.dto.DeleteReservationRes;
@@ -10,6 +14,7 @@ import com.shallwe.domain.reservation.exception.InvalidReservationException;
 import com.shallwe.domain.reservation.exception.InvalidUserException;
 import com.shallwe.domain.user.domain.User;
 import com.shallwe.domain.user.domain.repository.UserRepository;
+import com.shallwe.domain.user.exception.InvalidPhoneNumberException;
 import com.shallwe.global.config.security.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,79 +32,55 @@ public class ReservationServiceImpl {
 
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
-//    private final GiftRepository giftRepository;
+    private final ExperienceGiftRepository experienceGiftRepository;
 
-
-    //id 로 reservation 가져오기
-    private Reservation getReservation(Long id) {
-        Reservation reservation = reservationRepository.findById(id).orElseThrow(InvalidReservationException::new);
-
-        return reservation;
+    private ReservationResponse getReservation(Long id) {
+        return reservationRepository.findById(id).map(ReservationResponse::toDto).orElseThrow(InvalidReservationException::new);
     }
 
     @Transactional
-    public ReservationResponse addReservation(final ReservationRequest reservationRequest, final UserPrincipal userPrincipal) {
-        User sendUser = userRepository.findById(userPrincipal.getId()).orElseThrow(InvalidUserException::new);
-        //#Todo 전화번호로 유저 검색해서, 해당 유저의 전화번호랑 이름을 receiver , phone num 으로 저장해야함.
-        //User receiveUser = userRepository.findby
-        Reservation reservation = ReservationRequest.toEntity(reservationRequest, sendUser);
+    public ReservationResponse addReservation( ReservationRequest reservationRequest,  UserPrincipal userPrincipal) {
+        Reservation reservation = ReservationRequest.toEntity(
+                reservationRequest,
+                userRepository.findById(userPrincipal.getId())
+                        .orElseThrow(InvalidUserException::new),
+                userRepository.findByPhoneNumber(reservationRequest.getPhoneNumber())
+                        .orElseThrow(InvalidPhoneNumberException::new),
+                experienceGiftRepository.findByExperienceGiftId(reservationRequest.getExperienceGiftId())
+                        .orElseThrow(ExperienceGiftNotFoundException::new));
+
         Reservation savedReservation = reservationRepository.save(reservation);
         return ReservationResponse.toDto(savedReservation);
-
     }
 
     public List<ReservationResponse> findUserReservation(UserPrincipal userPrincipal) {
-        List<ReservationResponse> reservationRes = new ArrayList<>();
-        List<Reservation> userReservations = reservationRepository.findAllByUserId(userPrincipal.getId());
-
-        for (Reservation userreservation : userReservations) {
-            ReservationResponse reservationResponse = ReservationResponse.fromReservation(userreservation);
-            reservationRes.add(reservationResponse);
-        }
-        return reservationRes;
+        return reservationRepository.findAllBySenderId(userPrincipal.getId())
+                .stream().map(ReservationResponse::toDto).collect(Collectors.toList());
     }
 
     public List<ReservationResponse> getAllReservation() {
-        List<Reservation> reservations = reservationRepository.findAll();
-        List<ReservationResponse> reservationRes = new ArrayList<>();
-        for (Reservation reservation : reservations) {
-            ReservationResponse reservationResponse1 =  ReservationResponse.fromReservation(reservation);
-            reservationRes.add(reservationResponse1);
-        }
-        return reservationRes;
+        return reservationRepository.findAll().stream().map(ReservationResponse::toDto).collect(Collectors.toList());
     }
-
-    @Transactional
-    public ReservationResponse updateReservation(UpdateReservationReq updateReq, UserPrincipal userPrincipal) {
-        // 로그인한 사용자 ID와 예약 ID로 예약 찾기
-        Optional<Reservation> foundReservation =
-                reservationRepository.findByUserIdAndId(userPrincipal.getId(), updateReq.getId());
-        // 예약을 찾은 후 업데이트 및 저장, 아니면 예외 처리
-        Reservation updatedReservation = foundReservation.map(reservation -> {
-            reservation.updateReservation(updateReq);
-            return reservationRepository.save(reservation);
-        }).orElseThrow(InvalidReservationException::new);
-        // DTO로 변환 후 반환
-        return ReservationResponse.toDto(updatedReservation);
     //추가
     public List<ReservationResponse> getCurrentGiftReservation(ExperienceGift experienceGiftId){
         experienceGiftRepository.findById(experienceGiftId.getExperienceGiftId()).orElseThrow(ExperienceGiftNotFoundException::new);
         return reservationRepository.findByExperienceGift(experienceGiftId).stream().map(ReservationResponse::toDto).collect(Collectors.toList());
     }
 
-
     @Transactional
-    public Reservation cancelReservation(Long id) {
-        Reservation reservation = getReservation(id);
-        // reservation.cancelReservation();
-        return reservation;
+    public ReservationResponse updateReservation(UpdateReservationReq updateReq, UserPrincipal userPrincipal) {
+        Reservation updateReservation= reservationRepository.findBySenderIdAndId(userPrincipal.getId(),updateReq.getId()).map(
+                reservation -> {reservation.updateReservation(updateReq);
+                return reservationRepository.save(reservation);}
+        ).orElseThrow(InvalidReservationException::new);
+        return ReservationResponse.toDto(updateReservation);
     }
 
     @Transactional
     public DeleteReservationRes deleteReservation(UserPrincipal userPrincipal, Long id) {
-        Optional<Reservation> foundReservation = reservationRepository.findByUserIdAndId(userPrincipal.getId(),id);
-        foundReservation.ifPresent(reservationRepository::delete);
-        foundReservation.orElseThrow(InvalidReservationException::new);
+        Optional<Reservation> foundReservation = reservationRepository.findBySenderIdAndId(userPrincipal.getId(), id);
+        foundReservation.ifPresentOrElse(reservationRepository::delete,
+                () -> {throw new InvalidReservationException();});
         return DeleteReservationRes.toDTO();
     }
 
