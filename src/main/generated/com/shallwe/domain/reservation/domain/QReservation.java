804fbc3cc2ff892931c0c6df285718b068bfc16e
iseunghag <qw0802@naver.com>
2023-08-30 14:52:51 +0900

    #106 feat:oauth2,navercloud 암호화 추가

---
+++ src/main/generated/com/shallwe/domain/reservation/domain/QReservation.java
@@ -0,0 +1,82 @@
+package com.shallwe.domain.reservation.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReservation is a Querydsl query type for Reservation
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReservation extends EntityPathBase<Reservation> {
+
+    private static final long serialVersionUID = 661198765L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReservation reservation = new QReservation("reservation");
+
+    public final com.shallwe.domain.common.QBaseEntity _super = new com.shallwe.domain.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final DateTimePath<java.time.LocalDateTime> date = createDateTime("date", java.time.LocalDateTime.class);
+
+    public final com.shallwe.domain.experience_gift.domain.QExperienceGift experienceGift;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath invitationComment = createString("invitationComment");
+
+    public final StringPath invitationImg = createString("invitationImg");
+
+    public final ListPath<com.shallwe.domain.memory_photo.domain.MemoryPhoto, com.shallwe.domain.memory_photo.domain.QMemoryPhoto> memoryPhotos = this.<com.shallwe.domain.memory_photo.domain.MemoryPhoto, com.shallwe.domain.memory_photo.domain.QMemoryPhoto>createList("memoryPhotos", com.shallwe.domain.memory_photo.domain.MemoryPhoto.class, com.shallwe.domain.memory_photo.domain.QMemoryPhoto.class, PathInits.DIRECT2);
+
+    public final NumberPath<Long> persons = createNumber("persons", Long.class);
+
+    public final StringPath phoneNumber = createString("phoneNumber");
+
+    public final com.shallwe.domain.user.domain.QUser receiver;
+
+    public final EnumPath<ReservationStatus> reservationStatus = createEnum("reservationStatus", ReservationStatus.class);
+
+    public final com.shallwe.domain.user.domain.QUser sender;
+
+    //inherited
+    public final EnumPath<com.shallwe.domain.common.Status> status = _super.status;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QReservation(String variable) {
+        this(Reservation.class, forVariable(variable), INITS);
+    }
+
+    public QReservation(Path<? extends Reservation> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReservation(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReservation(PathMetadata metadata, PathInits inits) {
+        this(Reservation.class, metadata, inits);
+    }
+
+    public QReservation(Class<? extends Reservation> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.experienceGift = inits.isInitialized("experienceGift") ? new com.shallwe.domain.experience_gift.domain.QExperienceGift(forProperty("experienceGift"), inits.get("experienceGift")) : null;
+        this.receiver = inits.isInitialized("receiver") ? new com.shallwe.domain.user.domain.QUser(forProperty("receiver")) : null;
+        this.sender = inits.isInitialized("sender") ? new com.shallwe.domain.user.domain.QUser(forProperty("sender")) : null;
+    }
+
+}
+
