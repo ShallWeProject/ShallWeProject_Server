785ed04e09150dd9f5eb695558df8153599ae87f
hyunw9 <rkdgusdnr32@naver.com>
2023-10-12 00:13:37 +0900

    feat: 가능 시간 엔티티 개발
    가능 날짜를 외래키로 두어 연관관계 설정하였습니다.
    (가능 시간 테이블에 가능한 날짜는 없을 수 있지만, 만약 있다면 반드시 가능 날짜에 PK로 존재해야 함)

---
+++ src/main/java/com/shallwe/domain/reservation/domain/AvailableTime.java
@@ -0,0 +1,32 @@
+package com.shallwe.domain.reservation.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class AvailableTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "availableDate_id")
+    private AvailableDate availableDate;
+
+    private TimeSlot timeSlot;
+
+}
