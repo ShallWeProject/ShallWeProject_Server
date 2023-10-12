e5ee1b4e0a8b7add7087aacd1e6aa63d0113b9f3
박세진 <jinpark99@naver.com>
2023-10-12 10:07:30 +0900

    chore: 사소한 코드 수정

--- src/main/java/com/shallwe/domain/auth/exception/InvalidPasswordException.java
+++ src/main/java/com/shallwe/domain/auth/exception/InvalidPasswordException.java
@@ -2,7 +2,9 @@ package com.shallwe.domain.auth.exception;
 
 
 public class InvalidPasswordException extends RuntimeException {
+
     public InvalidPasswordException() {
         super("비밀번호가 일치하지 않습니다.");
     }
+
 }
--- src/main/java/com/shallwe/domain/shopowner/exception/InvalidShopOwnerException.java
+++ src/main/java/com/shallwe/domain/shopowner/exception/InvalidShopOwnerException.java
@@ -1,6 +1,7 @@
 package com.shallwe.domain.shopowner.exception;
 
 public class InvalidShopOwnerException extends RuntimeException {
+
     public InvalidShopOwnerException() {
         super("사장의 정보가 유효하지 않습니다.");
     }
