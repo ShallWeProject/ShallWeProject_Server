3b27cbe0a5a5cf2f6334ef4125606f570d0eb116
Park Sejin <jinpark99@naver.com>
2023-11-05 13:17:59 +0900

    feat: 사장 탈퇴 기능 구현

--- src/main/java/com/shallwe/domain/shopowner/application/ShopOwnerServiceImpl.java
+++ src/main/java/com/shallwe/domain/shopowner/application/ShopOwnerServiceImpl.java
@@ -1,10 +1,15 @@
 package com.shallwe.domain.shopowner.application;
 
 
+import com.shallwe.domain.auth.domain.Token;
+import com.shallwe.domain.auth.domain.repository.TokenRepository;
+import com.shallwe.domain.common.Status;
 import com.shallwe.domain.shopowner.domain.ShopOwner;
 import com.shallwe.domain.shopowner.domain.repository.ShopOwnerRepository;
 import com.shallwe.domain.shopowner.dto.ShopOwnerChangePasswordReq;
 import com.shallwe.domain.shopowner.exception.InvalidShopOwnerException;
+import com.shallwe.domain.user.domain.User;
+import com.shallwe.domain.user.exception.InvalidTokenException;
 import com.shallwe.global.config.security.token.UserPrincipal;
 import com.shallwe.global.payload.Message;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +24,9 @@ public class ShopOwnerServiceImpl implements ShopOwnerService {
 
     private final PasswordEncoder passwordEncoder;
     private final ShopOwnerRepository shopOwnerRepository;
+    private final TokenRepository tokenRepository;
 
+    @Override
     @Transactional
     public Message shopOwnerChangePassword(final UserPrincipal userPrincipal, final ShopOwnerChangePasswordReq shopOwnerChangePasswordReq) {
         ShopOwner shopOwner = shopOwnerRepository.findById(userPrincipal.getId())
@@ -29,6 +36,22 @@ public class ShopOwnerServiceImpl implements ShopOwnerService {
 
         return Message.builder()
                 .message("비밀번호가 변경되었습니다.").build();
+    }
+
+    @Override
+    @Transactional
+    public Message deleteCurrentShopOwner(UserPrincipal userPrincipal) {
+        ShopOwner shopOwner = shopOwnerRepository.findById(userPrincipal.getId())
+                .orElseThrow(InvalidShopOwnerException::new);
+        Token token = tokenRepository.findByUserEmail(userPrincipal.getEmail())
+                .orElseThrow(InvalidTokenException::new);
+
+        shopOwner.updateStatus(Status.DELETE);
+        tokenRepository.delete(token);
+
+        return Message.builder()
+                .message("사장 탈퇴가 완료되었습니다.")
+                .build();
     }
 
 }
