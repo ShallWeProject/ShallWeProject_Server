46274f0ef0d28479403c11d877f83ee572edd405
hyunw9 <rkdgusdnr32@naver.com>
2023-11-11 23:17:29 +0900

    feat: 예약 엔티티 수정

--- src/main/java/com/shallwe/domain/reservation/domain/Reservation.java
+++ src/main/java/com/shallwe/domain/reservation/domain/Reservation.java
@@ -5,12 +5,14 @@ import com.shallwe.domain.common.BaseEntity;
 import com.shallwe.domain.experiencegift.domain.ExperienceGift;
 import com.shallwe.domain.memoryphoto.domain.MemoryPhoto;
 import com.shallwe.domain.reservation.dto.UpdateReservationReq;
+import com.shallwe.domain.shopowner.domain.ShopOwner;
 import com.shallwe.domain.user.domain.User;
 import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import lombok.*;
 import org.hibernate.annotations.Where;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,13 +33,20 @@ public class Reservation extends BaseEntity {
     @JoinColumn(name = "experience_gift_id")
     private ExperienceGift experienceGift;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private ShopOwner owner;
+
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id")
     private User sender;
 
     private Long persons;
 
-    private LocalDateTime date;
+    private LocalDate date;
+
+    private LocalTime time;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id")
@@ -58,10 +67,14 @@ public class Reservation extends BaseEntity {
     public void updateReservation(UpdateReservationReq updateReq) {
         this.persons = Optional.ofNullable(updateReq.getPersons()).orElse(this.persons);
         this.date = Optional.ofNullable(updateReq.getDate()).orElse(this.date);
+        this.time = Optional.ofNullable(LocalTime.of(updateReq.getTime(),0)).orElse(this.time);
         this.receiver = Optional.ofNullable(updateReq.getReceiver()).orElse(this.getReceiver());
         this.phoneNumber = Optional.ofNullable(updateReq.getPhone_number()).orElse(this.phoneNumber);
         this.invitationImg = Optional.ofNullable(updateReq.getInvitation_img()).orElse(this.invitationImg);
         this.invitationComment = Optional.ofNullable(updateReq.getInvitation_comment()).orElse(this.invitationComment);
     }
 
+    public int getHour(){
+        return time.getHour();
+    }
 }
