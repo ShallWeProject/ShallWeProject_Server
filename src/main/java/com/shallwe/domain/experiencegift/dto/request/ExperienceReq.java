2e3ea9f7357cb950a679ca89d82bd4cb74bbe58c
hyunw9 <rkdgusdnr32@naver.com>
2023-11-11 13:24:40 +0900

    feat: 경험선물 추가 DTO 변경
    요청 값 기준으로 available date, timeslot 생성 -> 경험 선물 추가

--- src/main/java/com/shallwe/domain/experiencegift/dto/request/ExperienceReq.java
+++ src/main/java/com/shallwe/domain/experiencegift/dto/request/ExperienceReq.java
@@ -1,15 +1,22 @@
 package com.shallwe.domain.experiencegift.dto.request;
 
+import com.shallwe.domain.experiencegift.domain.AvailableDate;
 import com.shallwe.domain.experiencegift.domain.ExpCategory;
 import com.shallwe.domain.experiencegift.domain.ExperienceGift;
 import com.shallwe.domain.experiencegift.domain.SttCategory;
 import com.shallwe.domain.experiencegift.domain.Subtitle;
+import com.shallwe.domain.experiencegift.domain.TimeSlot;
+import com.shallwe.domain.experiencegift.domain.TimeStatus;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ExperienceReq {
+
     private String title;
     private String thumbnail;
     private Long price;
@@ -17,19 +24,43 @@ public class ExperienceReq {
     private Long sttCategoryId;
     private Long subtitleId;
     private String description;
-
+    private List<AvailableDateReq> availableDates;
 
     @Builder
-    public static ExperienceGift toEntity(ExperienceReq experienceReq,Subtitle subtitle, ExpCategory expCategory,SttCategory sttCategory ){
-        return ExperienceGift.builder()
-                .title(experienceReq.getTitle())
-                .thumbnail(experienceReq.getThumbnail())
-                .price(experienceReq.getPrice())
-                .description(experienceReq.getDescription())
-                .subtitle(subtitle)
-                .expCategory(expCategory)
-                .sttCategory(sttCategory)
-                .build();
-    }
+    public static ExperienceGift toEntity(ExperienceReq experienceReq, Subtitle subtitle, ExpCategory expCategory, SttCategory sttCategory) {
 
+        ExperienceGift experienceGift = ExperienceGift.builder()
+            .title(experienceReq.getTitle())
+            .thumbnail(experienceReq.getThumbnail())
+            .price(experienceReq.getPrice())
+            .description(experienceReq.getDescription())
+            .subtitle(subtitle)
+            .expCategory(expCategory)
+            .sttCategory(sttCategory)
+            .availableDates(new ArrayList<>())
+            .build();
+
+        List<AvailableDate> availableDates = experienceReq.getAvailableDates().stream()
+            .map(dateReq -> {
+                List<TimeSlot> timeSlots = dateReq.getTimeSlots().stream()
+                    .map(time -> TimeSlot.builder()
+                        .time(time)
+                        .status(TimeStatus.AVAILABLE)
+                        .build())
+                    .collect(Collectors.toList());
+
+                AvailableDate availableDate = AvailableDate.builder()
+                    .date(dateReq.getDate())
+                    .availableTimeslot(timeSlots)
+                    .experienceGift(experienceGift) // AvailableDate에 ExperienceGift 설정
+                    .build();
+
+                experienceGift.getAvailableDates().add(availableDate); // ExperienceGift에 AvailableDate 추가
+
+                return availableDate;
+            })
+            .toList();
+
+        return experienceGift;
+    }
 }
