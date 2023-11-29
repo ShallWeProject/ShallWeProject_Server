483e128a915758fa1f3783a6e47aa028655cb4be
Park Sejin <jinpark99@naver.com>
2023-11-29 23:20:03 +0900

    fix: UpdateReservationReq Request 형식 수정

--- src/main/java/com/shallwe/domain/reservation/application/ReservationServiceImpl.java
+++ src/main/java/com/shallwe/domain/reservation/application/ReservationServiceImpl.java
@@ -123,7 +123,7 @@ public class ReservationServiceImpl {
       UserPrincipal userPrincipal) {
 
     Reservation updateReservation = reservationRepository.findById(
-        updateReq.getId()).map(
+        updateReq.getReservationId()).map(
         reservation -> {
           reservation.updateReservation(updateReq);
           return reservationRepository.save(reservation);
