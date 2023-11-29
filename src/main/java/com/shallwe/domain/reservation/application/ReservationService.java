6dc9b6ac0970b4139c020348eefa6c022dca16de
hyunw9 <rkdgusdnr32@naver.com>
2023-11-30 01:51:28 +0900

    chore : swagger Scheme 변경

--- src/main/java/com/shallwe/domain/reservation/application/ReservationService.java
+++ src/main/java/com/shallwe/domain/reservation/application/ReservationService.java
@@ -1,13 +1,15 @@
 package com.shallwe.domain.reservation.application;
 
-import com.shallwe.domain.reservation.dto.ReservationResponse;
+import com.shallwe.domain.reservation.dto.ReservationIdOwnerRes;
+import com.shallwe.domain.reservation.dto.ReservationIdUserRes;
 import com.shallwe.global.config.security.token.UserPrincipal;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ReservationService {
 
-    List<ReservationResponse> getReservationByDate (UserPrincipal userPrincipal, Long giftId, LocalDate date);
+    List<ReservationIdOwnerRes> getReservationByDateOwner (UserPrincipal userPrincipal, Long giftId, LocalDate date);
+    List<ReservationIdUserRes> getReservationByDateUser (UserPrincipal userPrincipal, Long giftId, LocalDate date);
 
 }
 
