bc59a355c8f1471e6dff15e3ea1d4f99844683bd
hyunw9 <rkdgusdnr32@naver.com>
2023-12-01 20:28:57 +0900

    docs : @Schema로 설명 추가

--- src/main/java/com/shallwe/domain/reservation/dto/ReservationIdOwnerRes.java
+++ src/main/java/com/shallwe/domain/reservation/dto/ReservationIdOwnerRes.java
@@ -1,13 +1,12 @@
 package com.shallwe.domain.reservation.dto;
 
-import static com.amazonaws.services.ec2.model.PrincipalType.User;
+
 
 import com.shallwe.domain.reservation.domain.Reservation;
 import com.shallwe.domain.reservation.domain.ReservationStatus;
-import com.shallwe.domain.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 import java.util.Optional;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +14,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ReservationIdOwnerRes {
-
+  @Schema(description = "예약 Id")
   Long reservationId;
+
+  @Schema(description = "예약 상태 {WAITING(예약대기), BOOKED(예약중), COMFIRMED(예약확정), COMPLETED(이용완료), CANCELED(예약취소)}")
   ReservationStatus status;
+  
+  @Schema(description = "보내는이 이름(예약자)")
   String sender;
+
+  @Schema(description = "예약 시간")
   LocalTime time;
+
+  @Schema(description = "전화번호")
   String phoneNum;
+
+  @Schema(description = "예약 인원")
   Long person;
 
   @Builder
--- src/main/java/com/shallwe/domain/reservation/dto/ReservationIdUserRes.java
+++ src/main/java/com/shallwe/domain/reservation/dto/ReservationIdUserRes.java
@@ -2,13 +2,17 @@ package com.shallwe.domain.reservation.dto;
 
 import com.shallwe.domain.reservation.domain.Reservation;
 import com.shallwe.domain.reservation.domain.ReservationStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ReservationIdUserRes {
 
+  @Schema(description = "예약 Id")
   Long reservationId;
+
+  @Schema(description = "예약 상태 {WAITING(예약대기), BOOKED(예약중), COMFIRMED(예약확정), COMPLETED(이용완료), CANCELED(예약취소)}")
   ReservationStatus status;
 
   @Builder
