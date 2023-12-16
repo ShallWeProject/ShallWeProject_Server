2833b97f240ed63b130810ab46ea0fb11c6583d5
박세진 <jinpark99@naver.com>
2023-12-16 10:09:03 +0900

    feat: 사장 신분증/사업자등록증/통장사본 등록 API 구현

--- src/main/java/com/shallwe/domain/shopowner/application/ShopOwnerServiceImpl.java
+++ src/main/java/com/shallwe/domain/shopowner/application/ShopOwnerServiceImpl.java
@@ -17,12 +17,16 @@ import com.shallwe.domain.reservation.exception.InvalidAvailableTimeException;
 import com.shallwe.domain.reservation.exception.InvalidReservationException;
 import com.shallwe.domain.shopowner.domain.ShopOwner;
 import com.shallwe.domain.shopowner.domain.repository.ShopOwnerRepository;
+import com.shallwe.domain.shopowner.dto.ShopOwnerIdentificationReq;
 import com.shallwe.domain.shopowner.exception.InvalidShopOwnerException;
 import com.shallwe.domain.user.exception.InvalidTokenException;
 import com.shallwe.global.config.security.token.UserPrincipal;
 import com.shallwe.global.payload.Message;
+import com.shallwe.global.utils.AwsS3ImageUrlUtil;
 import jakarta.xml.bind.helpers.ValidationEventLocatorImpl;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,59 +37,74 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ShopOwnerServiceImpl implements ShopOwnerService {
 
-  private final PasswordEncoder passwordEncoder;
-  private final ShopOwnerRepository shopOwnerRepository;
-  private final TokenRepository tokenRepository;
-  private final ReservationRepository reservationRepository;
-  private final ExperienceGiftRepository experienceGiftRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ShopOwnerRepository shopOwnerRepository;
+    private final TokenRepository tokenRepository;
+    private final ReservationRepository reservationRepository;
+    private final ExperienceGiftRepository experienceGiftRepository;
 
-  @Override
-  @Transactional
-  public Message deleteCurrentShopOwner(UserPrincipal userPrincipal) {
-    ShopOwner shopOwner = shopOwnerRepository.findById(userPrincipal.getId())
-        .orElseThrow(InvalidShopOwnerException::new);
-    Token token = tokenRepository.findByUserEmail(userPrincipal.getEmail())
-        .orElseThrow(InvalidTokenException::new);
+    @Override
+    @Transactional
+    public Message deleteCurrentShopOwner(UserPrincipal userPrincipal) {
+        ShopOwner shopOwner = shopOwnerRepository.findById(userPrincipal.getId())
+                .orElseThrow(InvalidShopOwnerException::new);
+        Token token = tokenRepository.findByUserEmail(userPrincipal.getEmail())
+                .orElseThrow(InvalidTokenException::new);
 
-    shopOwner.updateStatus(Status.DELETE);
-    tokenRepository.delete(token);
+        shopOwner.updateStatus(Status.DELETE);
+        tokenRepository.delete(token);
 
-    return Message.builder()
-        .message("사장 탈퇴가 완료되었습니다.")
-        .build();
-  }
-
-  @Override
-  public List<ValidTimeSlotRes> getShopOwnerReservation(UserPrincipal userPrincipal,
-      Long giftId) {
-    ExperienceGift experienceGift = experienceGiftRepository.findByExperienceGiftId(giftId)
-        .orElseThrow(ExperienceGiftNotFoundException::new);
-    List<Reservation> reservationList = reservationRepository.findAllByExperienceGift(
-        experienceGift).orElseThrow(InvalidAvailableTimeException::new);
-
-    return reservationList.stream().map(reservation -> ValidTimeSlotRes.builder()
-        .reservationId(reservation.getId())
-        .date(reservation.getDate())
-        .time(reservation.getTime())
-        .build()).toList();
-  }
-
-  @Override
-  @Transactional
-  public Message confirmPayment(UserPrincipal userPrincipal, Long reservationId) {
-    Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
-        InvalidReservationException::new);
-    if (!reservation.getReservationStatus().equals(BOOKED)) {
-      return Message.builder()
-          .message("올바르지 않은 시도입니다.")
-          .build();
-    } else {
-      reservation.updateStatus(CONFIRMED);
-      reservationRepository.save(reservation);
-      return Message.builder()
-          .message("예약이 확정되었습니다.")
-          .build();
+        return Message.builder()
+                .message("사장 탈퇴가 완료되었습니다.")
+                .build();
     }
-  }
+
+    @Override
+    public List<ValidTimeSlotRes> getShopOwnerReservation(UserPrincipal userPrincipal,
+                                                          Long giftId) {
+        ExperienceGift experienceGift = experienceGiftRepository.findByExperienceGiftId(giftId)
+                .orElseThrow(ExperienceGiftNotFoundException::new);
+        List<Reservation> reservationList = reservationRepository.findAllByExperienceGift(
+                experienceGift).orElseThrow(InvalidAvailableTimeException::new);
+
+        return reservationList.stream().map(reservation -> ValidTimeSlotRes.builder()
+                .reservationId(reservation.getId())
+                .date(reservation.getDate())
+                .time(reservation.getTime())
+                .build()).toList();
+    }
+
+    @Override
+    @Transactional
+    public Message confirmPayment(UserPrincipal userPrincipal, Long reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
+                InvalidReservationException::new);
+        if (!reservation.getReservationStatus().equals(BOOKED)) {
+            return Message.builder()
+                    .message("올바르지 않은 시도입니다.")
+                    .build();
+        } else {
+            reservation.updateStatus(CONFIRMED);
+            reservationRepository.save(reservation);
+            return Message.builder()
+                    .message("예약이 확정되었습니다.")
+                    .build();
+        }
+    }
+
+    @Override
+    @Transactional
+    public Message uploadShopOwnerIdentification(ShopOwnerIdentificationReq shopOwnerIdentificationReq, UserPrincipal userPrincipal) {
+        ShopOwner shopOwner = shopOwnerRepository.findById(userPrincipal.getId())
+                .orElseThrow(InvalidShopOwnerException::new);
+
+        shopOwner.updateIdentification(AwsS3ImageUrlUtil.toUrl(shopOwnerIdentificationReq.getIdentification()));
+        shopOwner.updateBusinessRegistration(AwsS3ImageUrlUtil.toUrl(shopOwnerIdentificationReq.getBusinessRegistration()));
+        shopOwner.updateBankbook(AwsS3ImageUrlUtil.toUrl(shopOwnerIdentificationReq.getBankbook()));
+
+        return Message.builder()
+                .message("사장님 신분증/사업자등록증/통장사본 등록이 완료되었습니다.")
+                .build();
+    }
 
 }
