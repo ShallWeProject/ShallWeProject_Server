7c3e8a1a3f2252cae85a78329cae3f89fc774c57
hyunw9 <rkdgusdnr32@naver.com>
2024-01-08 16:33:57 +0900

    fix: 충돌 해결

--- src/main/java/com/shallwe/domain/reservation/domain/repository/ReservationQuerydslRepositoryImpl.java
+++ src/main/java/com/shallwe/domain/reservation/domain/repository/ReservationQuerydslRepositoryImpl.java
@@ -30,53 +30,53 @@ public class ReservationQuerydslRepositoryImpl implements ReservationQuerydslRep
     @Override
     public List<Reservation> findReservationsByPhoneNumberAndReservationStatusIn(String phoneNumber) {
         return queryFactory
-                .selectFrom(reservation)
-                .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
-                .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
-                .leftJoin(reservation.experienceGift.experienceCategory, experienceCategory).fetchJoin()
-                .leftJoin(reservation.experienceGift.situationCategory, situationCategory).fetchJoin()
-                .leftJoin(reservation.sender, new QUser("sender")).fetchJoin()
-                .where(
-                        reservation.phoneNumber.eq(phoneNumber)
-                                .and(reservation.reservationStatus.in(ReservationStatus.BOOKED, ReservationStatus.COMPLETED))
-                )
-                .orderBy(reservation.reservationStatus.asc()).orderBy(reservation.date.desc())
-                .fetch();
+            .selectFrom(reservation)
+            .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
+            .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
+            .leftJoin(reservation.experienceGift.experienceCategory, experienceCategory).fetchJoin()
+            .leftJoin(reservation.experienceGift.situationCategory, situationCategory).fetchJoin()
+            .leftJoin(reservation.sender, new QUser("sender")).fetchJoin()
+            .where(
+                reservation.phoneNumber.eq(phoneNumber)
+                    .and(reservation.reservationStatus.in(ReservationStatus.BOOKED, ReservationStatus.COMPLETED))
+            )
+            .orderBy(reservation.reservationStatus.asc()).orderBy(reservation.date.desc())
+            .fetch();
     }
 
     @Override
     public List<Reservation> findReservationsBySenderAndReservationStatusIn(User sender) {
         return queryFactory
-                .selectFrom(reservation)
-                .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
-                .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
-                .leftJoin(reservation.experienceGift.experienceCategory, experienceCategory).fetchJoin()
-                .leftJoin(reservation.experienceGift.situationCategory, situationCategory).fetchJoin()
-                .leftJoin(reservation.receiver, new QUser("receiver")).fetchJoin()
-                .where(
-                        reservation.sender.eq(sender)
-                                .and(reservation.reservationStatus.in(ReservationStatus.BOOKED, ReservationStatus.COMPLETED))
-                )
-                .orderBy(reservation.reservationStatus.asc()).orderBy(reservation.date.desc())
-                .fetch();
+            .selectFrom(reservation)
+            .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
+            .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
+            .leftJoin(reservation.experienceGift.experienceCategory, experienceCategory).fetchJoin()
+            .leftJoin(reservation.experienceGift.situationCategory, situationCategory).fetchJoin()
+            .leftJoin(reservation.receiver, new QUser("receiver")).fetchJoin()
+            .where(
+                reservation.sender.eq(sender)
+                    .and(reservation.reservationStatus.in(ReservationStatus.BOOKED, ReservationStatus.COMPLETED))
+            )
+            .orderBy(reservation.reservationStatus.asc()).orderBy(reservation.date.desc())
+            .fetch();
     }
 
     @Override
     public List<Reservation> findReservationsByDateAndUser(LocalDate date, User user) {
         return queryFactory
-                .selectFrom(reservation)
-                .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
-                .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
-                .leftJoin(reservation.memoryPhotos, memoryPhoto).fetchJoin()
-                .leftJoin(memoryPhoto.uploader).fetchJoin()
-                .leftJoin(reservation.sender).fetchJoin()
-                .leftJoin(reservation.receiver).fetchJoin()
-                .where(
-                        reservation.date.eq(date)
-                                .and(reservation.sender.eq(user).or(reservation.receiver.eq(user)))
-                                .and(reservation.reservationStatus.in(ReservationStatus.COMPLETED))
-                )
-                .fetch();
+            .selectFrom(reservation)
+            .leftJoin(reservation.experienceGift, experienceGift).fetchJoin()
+            .leftJoin(reservation.experienceGift.subtitle, subtitle).fetchJoin()
+            .leftJoin(reservation.memoryPhotos, memoryPhoto).fetchJoin()
+            .leftJoin(memoryPhoto.uploader).fetchJoin()
+            .leftJoin(reservation.sender).fetchJoin()
+            .leftJoin(reservation.receiver).fetchJoin()
+            .where(
+                reservation.date.eq(date)
+                    .and(reservation.sender.eq(user).or(reservation.receiver.eq(user)))
+                    .and(reservation.reservationStatus.in(ReservationStatus.COMPLETED))
+            )
+            .fetch();
     }
 
 }
