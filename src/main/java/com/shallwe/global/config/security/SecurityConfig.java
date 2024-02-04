19267f8d8ef6853090b3cca442858e604ce95b61
hyunw9 <rkdgusdnr32@naver.com>
2024-02-04 22:27:41 +0900

    feat : security path permit

--- src/main/java/com/shallwe/global/config/security/SecurityConfig.java
+++ src/main/java/com/shallwe/global/config/security/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/error", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js")
                                 .permitAll()
-                        .requestMatchers("/management/**")
+                        .requestMatchers("/management/**","/graph/**")
                                  .permitAll()
                         .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
                                 .permitAll()
