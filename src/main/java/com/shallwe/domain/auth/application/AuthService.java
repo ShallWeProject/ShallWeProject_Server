1fd9650ccbe3d25b1742ba37549f05ae9f043688
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-03-03 09:53:02 +0900

    fix: 애플 로그인 로직 수정 (#280)
    * feat: 외부 API 호출 툴 Feign 설정
    
    * feat: Apple 로그인 구현 + 회원가입 Reqeust Provider 추가
    
    * fix: Claims sub 항목 빼서 쓰게 수정
    
    * fix: AuthorizationCode Request 제거
    
    * fix: 애플 로그인 로직 수정

--- src/main/java/com/shallwe/domain/auth/application/AuthService.java
+++ src/main/java/com/shallwe/domain/auth/application/AuthService.java
@@ -73,34 +73,6 @@ public class AuthService {
     }
 
     @Transactional
-    public AuthRes appleSignUp(AppleSignUpReq appleSignUpReq) {
-        Claims claims = appleJwtUtils.getClaimsBy(appleSignUpReq.getIdentityToken());
-        String providerId = claims.get("sub").toString();
-
-        if (userRepository.existsByProviderId(providerId)) {
-            throw new AlreadyExistsProviderIdException();
-        }
-
-        String appleRefreshToken = appleJwtUtils.getAppleToken(appleSignUpReq.getAuthorizationCode());
-        AppleToken appleTokenReq = AppleToken.builder()
-                .providerId(providerId)
-                .refreshToken(appleRefreshToken)
-                .build();
-        appleTokenRepository.save(appleTokenReq);
-
-        User newUser = User.builder()
-                .providerId(providerId)
-                .provider(Provider.APPLE)
-                .email(appleSignUpReq.getEmail())
-                .password(passwordEncoder.encode(providerId))
-                .role(Role.USER)
-                .build();
-        userRepository.save(newUser);
-
-        return getUserAuthRes(newUser);
-    }
-
-    @Transactional
     public AuthRes signIn(final SignInReq signInReq) {
         User user = userRepository.findByEmailAndStatus(signInReq.getEmail(), Status.ACTIVE)
                 .orElseThrow(InvalidUserException::new);
@@ -116,7 +88,29 @@ public class AuthService {
         Claims claims = appleJwtUtils.getClaimsBy(appleSignInReq.getIdentityToken());
         String providerId = claims.get("sub").toString();
 
-        User user = userRepository.findByProviderId(providerId).orElseThrow(InvalidProviderIdException::new);
+        Optional<User> optionalUser = userRepository.findByProviderId(providerId);
+
+        if (optionalUser.isEmpty()) {
+            String appleRefreshToken = appleJwtUtils.getAppleToken(appleSignInReq.getAuthorizationCode());
+            AppleToken appleTokenReq = AppleToken.builder()
+                    .providerId(providerId)
+                    .refreshToken(appleRefreshToken)
+                    .build();
+            appleTokenRepository.save(appleTokenReq);
+
+            User newUser = User.builder()
+                    .providerId(providerId)
+                    .provider(Provider.APPLE)
+                    .password(passwordEncoder.encode(providerId))
+                    .role(Role.USER)
+                    .build();
+            userRepository.save(newUser);
+
+            optionalUser = Optional.of(newUser);
+        }
+
+        User user = optionalUser.get();
+
         if (user.getName() == null || user.getPhoneNumber() == null || user.getAge() == null || user.getGender() == null) {
             throw new UnRegisteredUserException();
         }
--- src/main/java/com/shallwe/domain/auth/exception/UnRegisteredUserException.java
+++ src/main/java/com/shallwe/domain/auth/exception/UnRegisteredUserException.java
@@ -3,7 +3,7 @@ package com.shallwe.domain.auth.exception;
 public class UnRegisteredUserException extends RuntimeException {
 
     public UnRegisteredUserException() {
-        super("회원가입이 완료되지 않은 유저입니다.");
+        super("유저정보 입력이 완료되지 않은 유저입니다.");
     }
 
 }
