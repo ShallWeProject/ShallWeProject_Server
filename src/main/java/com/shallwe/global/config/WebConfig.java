4ec0b82b05eff447960e7a0f10b18d3acf333165
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-03-15 06:18:42 +0900

    fix: WebConfig 수정 (#302)
    * fix: 구글/카카오 로그인 API 수정
    
    * fix: RefreshToken null 체크 로직 추가
    
    * fix: findByProvicderId Status 조건 추가
    
    * feat: 회원탈퇴 API 구현
    
    * feat: 인증번호 전송 API 테스트
    
    * fix: WebConfig 수정

--- src/main/java/com/shallwe/global/config/WebConfig.java
+++ src/main/java/com/shallwe/global/config/WebConfig.java
@@ -1,25 +1,27 @@
 package com.shallwe.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // url 패턴
-                .allowedOriginPatterns("*")
-                // todo: server url 생성 시 변경 필요
-                .allowedOrigins("https://api.shallwes.com/")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(),
-                        HttpMethod.HEAD.name(), HttpMethod.TRACE.name(), HttpMethod.OPTIONS.name()) // 허용 method
-                .allowedHeaders("Authorization", "Content-Type")// 허용 header
-                .allowCredentials(true);
-
-
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")// 허용 header
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
